41b617262fa6a56c03fe1e057254a314b5a16f63
RhesaFolakemi <adjoaboatemaa.mab@gmail.com>
2017-10-03 17:09:31 +0000

    did add some styles

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,3 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
-// window.onload = function () {
-//
-// var dps = []; // dataPoints
-// var chart = new CanvasJS.Chart("#chartContainer") {
-// 	title :{
-// 		text: "Exchange Rate"
-// 	},
-// 	axisY: {
-// 		includeZero: false
-// 	},
-// 	data: [{
-// 		type: "line",
-// 		dataPoints: dps
-// 	}]
-// });
-//
-// var xVal = 0;
-// var yVal = 100;
-// var updateInterval = 1000;
-// var dataLength = 20; // number of dataPoints visible at any point
-//
-// var updateChart = function (count) {
-//
-// 	count = count || 1;
